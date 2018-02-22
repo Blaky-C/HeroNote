1e2610235c7845fd22929cc61ff1e67464598496
sqs <googol.sqs@gmail.com>
2018-02-23 01:09:03 +0800

    fab behavior modified by sqs fab如果没有消失动作必然会遮挡信息

--- app/src/main/java/com/example/heronote/util/ScrollAwareFABBehavior.java
+++ app/src/main/java/com/example/heronote/util/ScrollAwareFABBehavior.java
@@ -1,40 +1,146 @@
 package com.example.heronote.util;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewCompat;
+import android.support.v4.view.animation.FastOutLinearInInterpolator;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 import android.util.AttributeSet;
 import android.view.View;
 
 /**
- * Created by SQS on 2018/2/17.
+ * Created by mac_xihao on 17/7/3.
  */
 
-public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior{
+public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
+
+
+    /**
+     * 因为是在XML中使用app:layout_behavior定义静态的这种行为,
+     * 必须实现一个构造函数使布局的效果能够正常工作。
+     * 否则 Could not inflate Behavior subclass error messages.
+     *
+     * @param context
+     * @param attrs
+     */
     public ScrollAwareFABBehavior(Context context, AttributeSet attrs) {
         super();
     }
 
+    /**
+     * 处理垂直方向上的滚动事件
+     *
+     * @param coordinatorLayout
+     * @param child
+     * @param directTargetChild
+     * @param target
+     * @param nestedScrollAxes
+     * @return
+     */
     @Override
-    public boolean onStartNestedScroll(final CoordinatorLayout coordinatorLayout, final FloatingActionButton child,
-                                       final View directTargetChild, final View target, final int nestedScrollAxes) {
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
+                                       FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+
         // Ensure we react to vertical scrolling
-        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
-                || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL ||
+                super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target,
+                        nestedScrollAxes);
     }
 
+    /**
+     * 检查Y的位置，并决定按钮是否动画进入或退出
+     *
+     * @param coordinatorLayout
+     * @param child
+     * @param target
+     * @param dxConsumed
+     * @param dyConsumed
+     * @param dxUnconsumed
+     * @param dyUnconsumed
+     */
     @Override
-    public void onNestedScroll(final CoordinatorLayout coordinatorLayout, final FloatingActionButton child,
-                               final View target, final int dxConsumed, final int dyConsumed,
-                               final int dxUnconsumed, final int dyUnconsumed) {
-        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
-        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
+                               View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
+                dyUnconsumed);
+
+        if (dyConsumed > 10 && child.getVisibility() == View.VISIBLE) {
             // User scrolled down and the FAB is currently visible -> hide the FAB
-            child.hide();
-        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            //执行隐藏的动画
+            hide(child);
+        } else if (dyConsumed < -10 && child.getVisibility() != View.VISIBLE) {
             // User scrolled up and the FAB is currently not visible -> show the FAB
-            child.show();
+            //执行显示的动画
+            show(child);
         }
+
+    }
+
+    /**
+     * 显示的动画
+     */
+    private void show(final View view) {
+        view.animate().cancel();
+
+        // If the view isn't visible currently, we'll animate it from a single pixel
+        view.setAlpha(0f);
+        view.setScaleY(0f);
+        view.setScaleX(0f);
+
+        view.animate()
+                .scaleX(1f)
+                .scaleY(1f)
+                .alpha(1f)
+                .setDuration(200)
+                .setInterpolator(new LinearOutSlowInInterpolator())
+                .setListener(new AnimatorListenerAdapter() {
+
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        view.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+
+                    }
+                });
+    }
+
+    /**
+     * 隐藏的动画
+     */
+    private void hide(final View view) {
+        view.animate().cancel();
+        view.animate()
+                .scaleX(0f)
+                .scaleY(0f)
+                .alpha(0f)
+                .setDuration(200)
+                .setInterpolator(new FastOutLinearInInterpolator())
+                .setListener(new AnimatorListenerAdapter() {
+                    private boolean mCancelled;
+
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        view.setVisibility(View.VISIBLE);
+                        mCancelled = false;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        mCancelled = true;
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        if (!mCancelled) {
+                            view.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
     }
 }
